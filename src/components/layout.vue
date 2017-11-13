1341da6f9ffb5d4348a02dcdd2f35c11c2a00a45
labnize <zhulixh@126.com>
2017-11-13 20:01:28 +0800

    element基础布局

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -1,31 +1,98 @@
 <template>
   <div class="layout">
-    <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item index="1">菜单一</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">菜单二</template>
-        <el-menu-item index="2-1">子菜单一</el-menu-item>
-        <el-menu-item index="2-2">子菜单二</el-menu-item>
-        <el-menu-item index="2-3">子菜单三</el-menu-item>
-      </el-submenu>
-      <el-menu-item index="3"><a href="https://july.life/" target="_blank">July's Blog</a></el-menu-item>
-    </el-menu>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-row class="tac">
+            <el-col :span="24">
+              <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b">
+                <el-menu-item index="1">
+                  <i class="el-icon-menu"></i>
+                  <span slot="title">导航二</span>
+                </el-menu-item>
+                <el-menu-item index="2">
+                  <i class="el-icon-setting"></i>
+                  <span slot="title">导航三</span>
+                </el-menu-item>
+                <el-submenu index="3">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>导航一</span>
+                  </template>
+                  <el-menu-item index="1-1">选项1</el-menu-item>
+                  <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-submenu>
+              </el-menu>
+            </el-col>
+          </el-row>
+        </el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'layout',
-  data() {
-    return {
-      activeIndex: '1',
-      activeIndex2: '1'
-    };
-  },
-  methods: {
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
+  export default {
+    name: 'layout',
+
+    data () {
+      return {}
+    },
+    methods: {
+      handleOpen (key, keyPath) {
+        console.log(key, keyPath)
+      },
+      handleClose (key, keyPath) {
+        console.log(key, keyPath)
+      }
     }
   }
-};
 </script>
+
+<style>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    /*background-color: #D3DCE6;*/
+    /*color: #333;*/
+    /*text-align: center;*/
+    /*line-height: 200px;*/
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import ElementUI from 'element-ui';
 import 'normalize.css';
-import 'element-ui/lib/theme-default/index.css';
+import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import App from './app';
 
