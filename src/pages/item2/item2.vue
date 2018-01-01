ac6b5db7dbd2ac1be86f440ca6abff9d683d4844
labnize <zhulixh@126.com>
2018-01-01 16:45:32 +0800

    eslint修复

--- src/pages/item2/item2.vue
+++ src/pages/item2/item2.vue
@@ -7,49 +7,46 @@
         <el-table-column
           prop="date"
           label="日期"
-          width="180">
-        </el-table-column>
+          width="180"/>
         <el-table-column
           prop="name"
           label="姓名"
-          width="180">
-        </el-table-column>
+          width="180"/>
         <el-table-column
           prop="address"
-          label="地址">
-        </el-table-column>
+          label="地址"/>
       </el-table>
     </div>
   </layout>
 </template>
 
 <script>
-  import Layout from 'components/layout';
+import Layout from 'components/layout';
 
-  export default {
-    components: {
-      Layout
-    },
-    data() {
-      return {
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
-      };
-    }
-  };
+export default {
+  components: {
+    Layout
+  },
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    };
+  }
+};
 </script>
